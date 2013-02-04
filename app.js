3e170c4aff957b56a9dcef351c3b0a7acb753068
luchenue <luchenue@gmail.com>
2013-02-05 01:40:24 +0800

    代码整理，加入页面代理模块

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , proxy = require('./routes/proxy')
   , services = require('./routes/service')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , url = require('url');
 
-BG = require('monitor/background.js')
+BG = require('./lib/background.js')
+dao = require("./lib/dao.js");
 $ = require('jquery');
 
 var app = express();
+
+var MemoryStore = express.session.MemoryStore;
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.cookieParser()); 
+  app.use(express.session({
+		secret: 'mon',
+		store:new MemoryStore({ reapInterval: 60000 * 10 }) 
+	}));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -42,14 +46,23 @@ WATCHDOG_TOLERANCE = 12E4;
 
 var sqlite3 = require('sqlite3').verbose();
 db = new sqlite3.Database('databases/monitor.db');
-dao = require("monitor/dao.js");
 
 app.get('/', routes.index);
-app.get('/users', user.list);
-
 app.get('/services/:method',function(req,res){
   services[req.params.method](req, res);
 });
+
+app.all('/proxy/http*',proxy.proxy)
+app.all('*',function(req, res, next){
+  if(req.headers.referer){
+    var ref = url.parse(req.headers.referer);
+    if(ref.host==req.headers.host && ref.pathname.indexOf('/proxy/http') == 0){
+		return proxy.handleUnknown(req, res)
+	}
+  }
+  next();
+});
+
 
 BG.watchdog();
 
